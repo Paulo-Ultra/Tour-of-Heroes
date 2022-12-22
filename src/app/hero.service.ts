bc1a6f0bd232f29ba9edb6edf3bc00bcf29a85ce
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-12-21 23:36:34 -0300

    Service implementada

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero.model';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  getHeroes(): Hero[]{
+    return HEROES;
+  }
 }
